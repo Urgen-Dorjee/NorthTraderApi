a27cbd38be6f4959c582e931c369914bf880e550
Urgen Dorjee <urgen0240@gmail.com>
2022-06-10 21:09:26 +0530

    Updated swagger configuration

--- NorthTraderAPI/Controllers/CustomersController.cs
+++ NorthTraderAPI/Controllers/CustomersController.cs
@@ -7,6 +7,7 @@ namespace NorthTraderAPI.Controllers
 
     [ApiController]
     [Route("api/[controller]")]
+    [Produces("application/json")]
     public class CustomersController : ControllerBase
     {
         private readonly ICustomerServices _customerContext;
@@ -19,19 +20,44 @@ namespace NorthTraderAPI.Controllers
             _logger = logger;
         }
 
+        /// <summary>
+        /// Display a list of all Customers
+        /// </summary>
+        /// <returns>It returns all the customer information from the database</returns>
         [HttpGet]
         public async Task<List<Customer>> GetAllCustomersAsync()
         {
             return await _customerContext.GetAllCustomersAsync();
         }
 
+        /// <summary>
+        /// Retrieving a Customer with specified ID
+        /// </summary>
+        /// <param name="id"></param>
+        /// <returns>It returns a customer info from database</returns>
+        /// <exception cref="KeyNotFoundException"></exception>
+
         [HttpGet("{id}")]
         public async Task<Customer?> GetCustomerAsync(string id)
         {
-            return await _customerContext.GetCustomerAsync(id);
-        }
+            _logger.LogInformation($"Retrieving Customer Info of ID: {id}");
+            var customer = await _customerContext.GetCustomerAsync(id);
+            if (customer is null)
+            {
+                throw new KeyNotFoundException($"Did not find a user of ID: {id}");
+            }
+            return customer;
 
+        }
+        /// <summary>
+        /// Adding a Customer to a database
+        /// </summary>
+        /// <param name="customer"></param>
+        /// <param name="cancel"></param>
+        /// <returns>A newly created customer data</returns>
         [HttpPost]
+        [ProducesResponseType(StatusCodes.Status201Created)]
+        [ProducesResponseType(StatusCodes.Status400BadRequest)]
         public async Task<Customer?> AddCustomer([FromBody] Customer customer, CancellationToken cancel)
         {
             if (!ModelState.IsValid) return customer;
--- NorthTraderAPI/Program.cs
+++ NorthTraderAPI/Program.cs
@@ -1,3 +1,5 @@
+using System.Reflection;
+using Microsoft.OpenApi.Models;
 using Newtonsoft.Json;
 using NorthTraderAPI;
 using NorthTraderAPI.DataServices;
@@ -13,10 +15,29 @@ builder.Host.ConfigureLogging(log =>
 
 builder.Services.AddControllers()
     .AddNewtonsoftJson(opt
-        =>opt.SerializerSettings.ReferenceLoopHandling
-            =ReferenceLoopHandling.Ignore);
+        => opt.SerializerSettings.ReferenceLoopHandling
+            = ReferenceLoopHandling.Ignore);
 builder.Services.AddEndpointsApiExplorer();
-builder.Services.AddSwaggerGen();
+builder.Services.AddSwaggerGen(opt =>
+{
+    opt.SwaggerDoc("v1", new OpenApiInfo
+    {
+        Version = "v1",
+        Title = "NorthwindTraderAPI",
+        Description = "Northwind Trading Co.",
+        Contact = new OpenApiContact
+        {
+            Name = "Urgen Dorjee",
+            Email = "urgen0240@gmail.com"
+        },
+        License = new OpenApiLicense
+        {
+            Name = "Northwind Trading Registered License"
+        }
+    });
+    var xmlFilename = $"{Assembly.GetExecutingAssembly().GetName().Name}.xml";
+    opt.IncludeXmlComments(Path.Combine(AppContext.BaseDirectory, xmlFilename));
+});
 builder.Services.AddApplicationInsightsTelemetry();
 builder.Services.AddDbContext<NorthwindDbContext>();
 builder.Services.AddScoped<ICustomerServices, CustomerServices>();
@@ -27,7 +48,6 @@ builder.Services.AddScoped<SampleDataSeeder>();
 
 var app = builder.Build();
 
-//var logger = app.Services.GetRequiredService<ILogger>();
 
 using var scope = app.Services.CreateScope();
 var dbInitializer = scope.ServiceProvider.GetRequiredService<SampleDataSeeder>();
@@ -39,6 +59,7 @@ if (app.Environment.IsDevelopment())
     app.UseSwaggerUI();
 }
 
+app.UseMiddleware<ExceptionHandlingMiddleware>();
 app.UseHttpsRedirection();
 
 app.UseAuthorization();
